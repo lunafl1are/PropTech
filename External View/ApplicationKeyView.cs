646ba90a3329f1d39420c0ac2dc05b7170eae4ad
lunafl1are <lunafl1are@protonmail.com>
2024-08-02 15:11:34 +0300

    update css

---
+++ External View/ApplicationKeyView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ICD.Base.Domain.External_View
+{
+    public class ApplicationKeyView
+    {
+        public long ApplicationKey { get; set; }
+        public long ApplicationTableKey { get; set; }
+
+        public string Alias { get; set; }
+        public string Name { get; set; }
+    }
+}
